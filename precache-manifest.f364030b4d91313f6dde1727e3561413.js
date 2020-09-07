e92c27bb1da76bdb5ac96df7b895a1bbb678165b
Sabin Tuitui <changemebeen@gmail.com>
2020-09-07 17:03:15 +0545

    Updates

---
+++ precache-manifest.f364030b4d91313f6dde1727e3561413.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5660b2206d81ce7e6f1ef44aa6b56f07",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "b8f4e6e1ed320f0e3b48",
+    "url": "/react-website/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "891764fdf7a14da4d9a8",
+    "url": "/react-website/static/css/main.e0e60939.chunk.css"
+  },
+  {
+    "revision": "b8f4e6e1ed320f0e3b48",
+    "url": "/react-website/static/js/2.20437e48.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/react-website/static/js/2.20437e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891764fdf7a14da4d9a8",
+    "url": "/react-website/static/js/main.c4df8e44.chunk.js"
+  },
+  {
+    "revision": "fadfad2d28377605b9ac",
+    "url": "/react-website/static/js/runtime-main.caee497b.js"
+  },
+  {
+    "revision": "be853c644e8dbe9eff12285053504d59",
+    "url": "/react-website/static/media/about-img.be853c64.jpg"
+  },
+  {
+    "revision": "9ce8aa204a21e69716018fee472b042c",
+    "url": "/react-website/static/media/app.9ce8aa20.png"
+  },
+  {
+    "revision": "3582747eea85f2ee647f0e94753fea89",
+    "url": "/react-website/static/media/graphic.3582747e.jpg"
+  },
+  {
+    "revision": "d826d69d567eb8a1f1b047ca98185688",
+    "url": "/react-website/static/media/home-img.d826d69d.png"
+  },
+  {
+    "revision": "d8f617def910f2f6e9d9706796972dbe",
+    "url": "/react-website/static/media/marketing.d8f617de.jpg"
+  },
+  {
+    "revision": "f6dee1b5714fceb98873e76c0a437387",
+    "url": "/react-website/static/media/ui.f6dee1b5.png"
+  },
+  {
+    "revision": "f12b349d6cd33e055572e3462fbc77c7",
+    "url": "/react-website/static/media/webdesign.f12b349d.png"
+  },
+  {
+    "revision": "57e269d772efb557362226367e9e2a63",
+    "url": "/react-website/static/media/webdevelopment.57e269d7.jpeg"
+  }
+]);
